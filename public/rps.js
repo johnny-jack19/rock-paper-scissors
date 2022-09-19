80b68c4c37a28b83e2c5e0f6b98789cb328da341
Jonathan Jackson <jacksonj9891@gmail.com>
2022-09-19 07:49:16 -0400

    First Commit

---
+++ public/rps.js
@@ -0,0 +1,82 @@
+const game = document.getElementById('game');
+const gameButtons = document.getElementsByClassName('game-button');
+
+for (let button of gameButtons) {
+    button.addEventListener('click', () => {
+        console.log(button.id);
+    });
+}
+
+let wins = 0;
+let losses = 0;
+let count = 0;
+let playerPick;
+let opponentPick;
+const opponentOptions = ['Rock', 'Paper', 'Scissors'];
+const gameLogic = {
+    Rock: {
+        Rock: 'Draw',
+        Paper: 'Loss',
+        Scissors: 'Win'
+    },
+    Paper: {
+        Rock: 'Win',
+        Paper: 'Draw',
+        Scissors: 'Loss'
+    },
+    Scissors: {
+        Rock: 'Loss',
+        Paper: 'Win',
+        Scissors: 'Draw'
+    }
+}
+
+function opening() {
+    opponentPick = opponentOptions[Math.floor(Math.random() * 3)];
+    game.innerHTML = (`
+    <div id="player-pick">
+        <button class="game-button Rock" id="Rock">Rock</button>
+        <button class="game-button Paper" id="Paper">Paper</button>
+        <button class="game-button Scissors" id="Scissors">Scissors</button>
+    </div>
+    `);
+    const gameButtons = document.getElementsByClassName('game-button');
+
+    for (let button of gameButtons) {
+        button.addEventListener('click', () => {
+            playerPick = button.id;
+            results();
+        });
+    }
+}
+
+function results() {
+    count++
+    let winResults = ['loss', 'loss'];
+    if (gameLogic[playerPick][opponentPick] == 'Win') {
+        wins++;
+        winResults[0] = 'winner';
+    } else if (gameLogic[playerPick][opponentPick] == 'Loss') {
+        losses++;
+        winResults[1] = 'winner';
+    }
+    game.innerHTML = (`
+    <div class="results">
+        <div class="pick"><p>You picked:</p><button class="game-button ${playerPick} ${winResults[0]}" disabled>${playerPick}</button></div>
+        <div class="pick"><p>Opponent picked:</p><button class="game-button ${opponentPick} ${winResults[1]}" disabled>${opponentPick}</button></div>
+    </div>
+    <div class="score">
+        <p>${gameLogic[playerPick][opponentPick]}</p>
+        <p>Total Games: ${count}</p>
+        <div class="row">
+            <p>Wins: ${wins}</p>
+            <p>Losses: ${losses}</p>
+            <p>Draws: ${count - wins - losses}</p>
+        </div>
+    </div>
+    <button onclick="opening()" class="restart">Restart</button>
+    `);
+}
+
+
+opening();
